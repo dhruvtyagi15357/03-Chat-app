1d43fcd93756c084f7eb30fe79c9e0f813739178
Dhruv Tyagi <dhruvtyagi01@gmail.com>
2024-04-14 17:34:05 +0530

    Done with UI, next up: Connect to Backend API

--- frontend/src/components/messages/MessageContainer.jsx
+++ frontend/src/components/messages/MessageContainer.jsx
@@ -1,22 +1,43 @@
 import React from "react";
 import Messages from "./Messages";
 import MessageInput from "./MessageInput";
+import { TiMessages } from "react-icons/ti";
 
 const MessageContainer = () => {
+  const noChat = true;
   return (
-    <div className="min-w-[450px] flex flex-col">
-      <>
-        {/* header */}
-        <div className="bg-slate-500 px-4 py-2 mb-2">
-          <span className="label-text">To: </span>{" "}
-          <span className="text-gray-900 font-bold">John Doe</span>
-        </div>
+    <div className="min-w-[450px] flex flex-col ">
+      {noChat ? (
+        <NoChatSelected />
+      ) : (
+        <>
+          {/* header */}
+          <div className="bg-slate-500 px-4 py-2 mb-2">
+            <span className="label-text">To: </span>{" "}
+            <span className="text-gray-900 font-bold">John Doe</span>
+          </div>
 
-        <Messages/>
-        <MessageInput/>
-      </>
+          <Messages />
+          <MessageInput />
+        </>
+      )}
     </div>
   );
 };
+
+const NoChatSelected = () => {
+  return (
+    <div className="flex flex-col items-center justify-center h-full w-full">
+      <div className="px-4 sm:text-lg md:text-xl text-gray-200 font-semibold flex flex-col items-center gap-2 text-center">
+        <h2 className="text-2xl font-bold">No chat selected</h2>
+        <p className="">
+          Welcome john doe
+        </p>
+        <p>Select a chat to start messaging</p>
+        <TiMessages className="text-3xl md:text-6xl text-center" />
+      </div>
+    </div>
+  );
+}
 
 export default MessageContainer;
--- frontend/src/components/messages/MessageInput.jsx
+++ frontend/src/components/messages/MessageInput.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
+import { BsSend } from "react-icons/bs";
 
 const MessageInput = () => {
   return (
-    <div>MessageInput</div>
-  )
-}
+    <form action="" className="px-4 my-3 pr-6">
+      <div className="w-full relative">
+        <input
+          type="text"
+          className="border text-sm rounded-lg block w-full p-2 5 bg-gray-700 border-gray-600 text-white"
+          placeholder="Start typing..."
+        />
+        <button className="absolute inset-y-0 end-0 flex items-center pe-3">
+          <BsSend/>
+        </button>
+      </div>
+    </form>
+  );
+};
 
-export default MessageInput
+export default MessageInput;
